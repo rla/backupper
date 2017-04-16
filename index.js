b013f1b7016ce11be844af6d1a7140e9089b5ba8
Raivo Laanemets <raivo@infdot.com>
2017-04-16 21:42:18 +0300

    Install CLI command, monthly snapshot to S3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 const fs = require('fs');
 const path = require('path');
 const yargs = require('yargs');
@@ -5,39 +6,59 @@ const Config = require('./lib/backup/config');
 const promisify = require('./lib/promisify');
 const tools = require('./lib/tools');
 const log = require('./lib/log');
+const tar = require('./lib/tools/tar');
+const s3 = require('./lib/s3');
 
 const readFile = promisify(fs.readFile);
+const unlink = promisify(fs.unlink);
 
 const argv = yargs
-    .option('command', {
-        alias: 'c',
-        describe: 'command to execute',
-        choices: ['backup', 'snapshot']
-    })
     .option('base', {
         alias: 'b',
         describe: 'base directory'
     })
-    .demandOption(['command', 'base'])
+    .demandOption(['base'])
     .argv;
 
-if (argv.command && argv.base) {
+const readConfig = async (base) => {
+    const configFile = path.join(base, 'config.json');
+    const configData = JSON.parse(await readFile(configFile, 'utf8'));
+    const config = new Config(base, configData.sites, configData.s3);
+    await config.validate();
+    return config;
+};
+
+const snapshot = async (base, config) => {
+    const sitesDirectory = path.join(base, 'sites');
+    const dateSuffix = new Date().toISOString().substring(0, 10);
+    const snapshotFile = path.join(base, 'sites-' + dateSuffix + '.tgz');
+    await tar.tarGzip(sitesDirectory, snapshotFile);
+    await s3.upload(snapshotFile, config.s3.bucket,
+        config.s3.accessKeyId, config.s3.secretAccessKey);
+    return unlink(snapshotFile);
+};
+
+const run = async (base) => {
+    const config = await readConfig(base);
+    await tools.report();
+    await config.execute();
+    const dayOfMonth = new Date().getDate();
+    log.info(`Current day of month is ${dayOfMonth}.`);
+    log.info(`Snapshot day of month is ${config.s3.dayOfMonth}.`);
+    if (dayOfMonth === config.s3.dayOfMonth) {
+        log.info('Taking snapshot.');
+        await snapshot(base, config);
+    }
+    if (config.error) {
+        throw new Error('Error occurred during backup.');
+    }
+};
+
+if (argv.base) {
     const base = path.resolve(argv.base);
     log.info(`Using command ${argv.command}.`);
     log.info(`Using base ${base}.`);
-
-    const configFile = path.join(base, 'config.json');
-    const run = async () => {
-        await tools.report();
-        const configData = JSON.parse(await readFile(configFile, 'utf8'));
-        const config = new Config(base, configData.sites);
-        await config.validate();
-        await config.execute();
-        if (config.error) {
-            throw new Error('Error occurred during backup.');
-        }
-    };
-    run().catch(async (err) => {
+    run(base).catch(async (err) => {
         await log.error(err);
         process.exit(1);
     });
